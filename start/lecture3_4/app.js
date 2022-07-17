e15a62bcd64060d137d3e41ec984b97bbd0bf526
syafiqoarashy <syafiqo.arashy@ui.ac.id>
2022-07-17 17:16:24 +0700

    completed lecture 3_4

--- start/lecture3_4/app.js
+++ start/lecture3_4/app.js
@@ -142,6 +142,28 @@ class App{
     handleController( controller ){
         if (controller.userData.selectPressed){
             controller.children[0].scale.z = 10;
+
+            this.workingMatrix.identity().extractRotation(controller.matrixWorld);
+
+            this.raycaster.ray.origin.setFromMatrixPosition(
+                controller.matrixWorld
+            );
+
+            this.raycaster.ray.direction.set(0,0,-1).applyMatrix4(
+                this.workingMatrix
+            );
+
+            const intersects = this.raycaster.intersectObjects(
+                this.room.children
+            );
+
+            if (intersects.length > 0){
+                intersects[0].object.add(this.highlight);
+                this.highlight.visible = true;
+                controller.children[0].scale.z = intersects[0].distance;
+            }else{
+                this.highlight.visible = false;
+            }
         }
     }
     
