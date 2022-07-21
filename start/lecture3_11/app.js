cd52fd5354e0df4e85255c9f41ddc81da7b984e4
syafiqoarashy <syafiqo.arashy@ui.ac.id>
2022-07-21 14:44:03 +0700

    completed 3_11

--- start/lecture3_11/app.js
+++ start/lecture3_11/app.js
@@ -142,7 +142,63 @@ class App{
         const btn = new ARButton( this.renderer, { onSessionStart, onSessionEnd } );
         
         //Add gestures here
-        
+        this.gestures = new ControllerGestures(this.renderer);
+
+        this.gestures.addEventListener('tap', (ev)=>{
+            console.log('tap');
+            self.ui.updateElement('info','tap');
+
+            if(!self.knight.object.visible){
+                self.knight.object.visible = true;
+                self.knight.object.position.sest(0, -0.3, -0.5).add(ev.position);
+                self.scene.add(self.knight.object);
+            }
+        });
+
+        this.gestures.addEventListener('swipe', (ev)=>{
+            console.log(ev);
+            self.ui.updateElement('info','swipe ${ev.direction}')
+            if (self.knight.object.visible){
+                self.knight.object.visible = false;
+                self.scene.remove(self.knight.object);
+            }
+        });
+
+        this.gestures.addEventListener('pan', (ev)=>{
+            console.log(ev);
+            if (ev.initialise !== undefined){
+                self.startPosition = self.knight.object.position.clone();
+            }else{
+                const pos = self.startPosition.clone().add(
+                    ev.delta.multiplyScalar(3)
+                );
+                self.knight.object.position.copy(pos);
+                self.ui.updateElement('info', 'pan x:${ev.delta.x.toFixed(3)} y:${ev.delta.y.toFixed(3)} z:${ev.delta.z.toFixed(3)}');
+            }
+        });
+
+        this.gestures.addEventListener('pinch', (ev)=>{
+            console.log(ev);
+            if (ev.initialise !== undefined){
+                self.startScale = self.knight.object.scale.clone();
+            }else{
+                const scale = self.startScale.clone().multiplyScalar(ev.scale);
+                self.knight.object.scale.copy(scale);
+                self.ui.updateElement('info','pinch delta:${ev.delta.toFixed(3)} scale:${ev.scale.toFixed(2)}');
+            }
+        });
+
+        this.gestures.addEventListener('rotate', (ev)=>{
+            console.log(ev);
+            if (ev.initialise !== undefined){
+                self.startQuaternion = self.knight.object.quaternion.clone();
+            }else{
+                self.knight.object.quaternion.copy(self.startQuaternion);
+                self.knight.object.rotateY(ev.theta);
+                self.ui.updateElement('info', 'rotate ${ev.theta.toFixed(3)}');
+            }
+        })
+
         this.renderer.setAnimationLoop( this.render.bind(this) );
     }
     
