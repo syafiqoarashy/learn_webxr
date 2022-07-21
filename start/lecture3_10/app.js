1cffe03a7b203f60632c1c4d14aa8a597162a713
syafiqoarashy <syafiqo.arashy@ui.ac.id>
2022-07-21 13:44:39 +0700

    completed 3_10

--- start/lecture3_10/app.js
+++ start/lecture3_10/app.js
@@ -105,7 +105,6 @@ class App{
         this.renderer.xr.enabled = true; 
         
         const self = this;
-        let controller;
         
         function onConnected( event ) {
             if (self.info === undefined){
@@ -133,13 +132,25 @@ class App{
         }
         
         function onSessionStart(){
-            
+            self.ui.mesh.position.set(0, -0.5, -1.1);
+            self.camera.add(self.ui.mesh);
         }
         
         function onSessionEnd(){
-            
+            self.camera.remove(self.ui.mesh);
         }
         
+        const btn = new ARButton(this.renderer, {onSessionStart, onSessionEnd
+        , sessionInit: {optionalFeatures: ['dom-overlay'], domOverlay: {
+            root: document.body
+        }}});
+
+        const controller = this.renderer.xr.getController(0);
+        controller.addEventListener('connected', onConnected);
+
+        this.scene.add(controller);
+        this.controller = controller;
+
         this.renderer.setAnimationLoop( this.render.bind(this) );
     }
     
--- start/lecture3_9/app.js
+++ start/lecture3_9/app.js
@@ -51,13 +51,14 @@ class App{
         const self = this;
         let controller;
 
-        function onSelect(){
+        function onSelect() {
             const material = new THREE.MeshPhongMaterial( {color: 0xFFFFFF * Math.random()});
             const mesh = new THREE.Mesh(self.geometry,material);
             mesh.position.set( 0, 0, - 0.3).applyMatrix4(controller.matrixWorld);
             mesh.quaternion.setFromRotationMatrix(controller.matrixWorld);
             self.scene.add(mesh);
             self.meshes.push(mesh);
+
         }
 
         const btn = new ARButton( this.renderer );
